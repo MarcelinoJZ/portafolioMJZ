7160cc2afcf42fd0bee3f1323e3d7ca236420436
MarcelinoJZ <46471683+MarcelinoJZ@users.noreply.github.com>
2024-11-08 08:19:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -33,9 +33,11 @@ function efectoHabilidades() {
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
         document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+        document.getElementById("python").classList.add("barra-progreso2");
+        document.getElementById("java").classList.add("barra-progreso3");
+        document.getElementById("javascript").classList.add("barra-progreso4");
+        document.getElementById("nodejs").classList.add("barra-progreso5");
+        document.getElementById("postgresql").classList.add("barra-progreso6");
+        document.getElementById("mysql").classList.add("barra-progreso7");
     }
-
 }
